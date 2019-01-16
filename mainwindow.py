abd2050fea0438c7e4e1ed536906648575b4ba91
igrekus <upload234@mail.ru>
2019-01-16 13:32:15 +0300

    MISC: fix fedit input format

--- mainwindow.py
+++ mainwindow.py
@@ -245,11 +245,11 @@ class MainWindow(QMainWindow):
     # action triggers
     @pyqtSlot()
     def on_actSettings_triggered(self):
-        data = [
+        data = (
             ('Параметр шума', self._domain._offset),
             ('Параметр частоты', self._domain._freqOffset),
             ('Параметр мощности', self._domain._ampOffset)
-        ]
+        )
         # TODO сменить единицу измерения частоты отстройки
         values = fedit(data=data, title='Настройки')
         if not values:
