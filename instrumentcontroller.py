ca523b33cd685bd284bdc3fa62cc574e65b01186
igrekus <upload234@mail.ru>
2019-02-12 18:39:43 +0300

    WIP: measure algorithm debug

--- instrumentcontroller.py
+++ instrumentcontroller.py
@@ -40,12 +40,15 @@ class InstrumentController:
             print(ex)
         print(f'analyzer: {self._analyzer}')
 
+        if self._analyzer:
+            self._analyzer.reset()
+
         return bool(self._analyzer)
 
     def test_sample(self):
         return True
 
-    def measure(self, params):
+    def new_measure(self, params):
         print(params)
 
         self._analyzer.reset()
@@ -65,40 +68,67 @@ class InstrumentController:
         amp = self._analyzer.calc_pow()
         freq = self._analyzer.calc_freq()
 
-        self._analyzer.send(f'SENS:FN:FREQ {int(freq)}')
+        print(cur)
+        print(amp)
+        print(freq)
 
-        self._analyzer.sense_freq_start(mode='FN', freq=int(params.f1))
-        self._analyzer.sense_freq_stop(mode='FN', freq=int(params.f2))
-        self._analyzer.sense_freq_det(mode='FN', value='NEV')   # NEV / ALW
+    def measure(self, params):
+        print(params)
 
-        self._analyzer.send(f'SENS:FN:FREQ:AUTO ON')
+        try:
+            # self._analyzer.reset()
+            self._analyzer.source_supply_voltage(supply=1, volt=params.v1)
+            self._analyzer.source_supply_status(supply=1, status='ON' if params.v1 != 0 else 'OFF')
+            self._analyzer.source_supply_voltage(supply=2, volt=params.v2)
+            self._analyzer.source_supply_status(supply=2, status='ON' if params.v2 != 0 else 'OFF')
 
-        print('*OPC?:', self._analyzer.operation_complete)
+            self._analyzer.source_tune_dut_voltage(volt=params.vc)
+            self._analyzer.source_tune_dut_status(status='ON' if params.vc != 0 else 'OFF')
 
-        self._analyzer.sense_corrections(mode='FN', corrections=params.corr)
-        self._analyzer.sense_averages(mode='FN', averages=params.aver)
-        self._analyzer.sense_ppd(mode='FN', value=0)
-        self._analyzer.sense_spur_omis(mode='FN', omission='OFF')
-        self._analyzer.sense_spur_threshold(mode='FN', threshold=10)
-        self._analyzer.sense_smo_status(mode='FN', status='OFF')
-        self._analyzer.sense_smo_aperture(mode='FN', aperture=1)
-        self._analyzer.sense_reset(mode='FN')
+            self._analyzer.sense_adc_rosc_source(source='INT')
+            self._analyzer.sense_mode(mode='FN')
 
-        self._analyzer.trigger_init()
+            self._analyzer.sense_freq_exec()
+            cur = self._analyzer.measure_supply_current(supply=1)
+            amp = self._analyzer.calc_pow()
+            freq = self._analyzer.calc_freq()
 
-        self._analyzer.calc_wait_average('NEXT,800')
+            # self._analyzer.send(f'SENS:FN:FREQ {freq}')
 
-        self._analyzer.system_error_all()
+            self._analyzer.sense_freq_start(mode='FN', freq=int(params.f1))
+            self._analyzer.sense_freq_stop(mode='FN', freq=int(params.f2))
+            self._analyzer.sense_freq_det(mode='FN', value='NEV')   # NEV / ALW
 
-        self._analyzer.status_questionable_condition()
+            self._analyzer.send(f'SENS:FN:FREQ:AUTO ON')
 
-        self._analyzer.calc_prel_averages(mode='FN')
-        self._analyzer.calc_prel_corrections(mode='FN')
+            print('*OPC?:', self._analyzer.operation_complete)
 
-        freqs = self._analyzer.calc_trace_freq(mode='FN')
-        amps = self._analyzer.calc_trace_noise(mode='FN')
+            self._analyzer.sense_corrections(mode='FN', corrections=params.corr)
+            self._analyzer.sense_averages(mode='FN', averages=params.aver)
+            self._analyzer.sense_ppd(mode='FN', value=0)
+            self._analyzer.sense_spur_omis(mode='FN', omission='OFF')
+            self._analyzer.sense_spur_threshold(mode='FN', threshold=10)
+            self._analyzer.sense_smo_status(mode='FN', status='OFF')
+            self._analyzer.sense_smo_aperture(mode='FN', aperture=1)
+            self._analyzer.sense_reset(mode='FN')
 
-        self._analyzer.system_error_all()
+            self._analyzer.trigger_init()
+
+            self._analyzer.calc_wait_average('NEXT,800')
+
+            self._analyzer.system_error_all()
+
+            self._analyzer.status_questionable_condition()
+
+            self._analyzer.calc_prel_averages(mode='FN')
+            self._analyzer.calc_prel_corrections(mode='FN')
+
+            freqs = self._analyzer.calc_trace_freq(mode='FN')
+            amps = self._analyzer.calc_trace_noise(mode='FN')
+
+            self._analyzer.system_error_all()
+        except Exception as ex:
+            print(ex)
 
         return freqs, amps, freq, amp, cur
 
